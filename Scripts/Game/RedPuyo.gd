65e65437e6bd1e0d03790a0773394fc64dd90c78
Gianfranco <cicciomesere@gmail.com>
2023-08-17 16:51:36 +0100

    Puyos now fall in a grid pattern

--- Scripts/Game/RedPuyo.gd
+++ Scripts/Game/RedPuyo.gd
@@ -1,15 +1,19 @@
 extends Area2D
 
-var tile_size = 64
+var tile_size = 58
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	processPuyo()
-
-func processPuyo():
 	pass
+
+
+func _on_gravity_timer_timeout():
+	if(!$RayBottom.is_colliding()):
+		position += Vector2.DOWN * tile_size
+
