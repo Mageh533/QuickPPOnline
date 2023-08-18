b172b25169204aed62f8cc73f4832daf9e8c01dd
Gianfranco <cicciomesere@gmail.com>
2023-08-18 19:13:01 +0100

    Script for popping puyos WIP
    as well as probably other game logic, not finished

---
+++ Scripts/Game/PuyoGame.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+var puyosObjectArray = []
+var puyosToPop = [] # To pop all of them at the same time
+var checkPopTimer = false
+
+func _ready():
+	pass
+
+func _process(_delta):
+	connectPuyosToGame()
+
+# Connects their signals
+func connectPuyosToGame():
+	puyosObjectArray = get_tree().get_nodes_in_group("Puyos")
+	for puyo in puyosObjectArray:
+		if !puyo.active:
+			puyo.active = true
+			puyo.puyoConnected.connect(_on_puyo_connected)
+
+# If any puyo emits the signal they are connected it starts the popping timer
+func _on_puyo_connected():
+	if !checkPopTimer:
+		checkPopTimer = true
+		$PoppingTimer.start()
+
+# Function checks if 4 or more puyos are connected, if so runs their pop function
+func _on_popping_timer_timeout():
+	var connectedPuyos = []
+	for puyo in puyosObjectArray:
+		connectedPuyos.clear()
+		if puyo.connected.size() > 0 and !puyosToPop.has(puyo):
+			connectedPuyos.append(puyo)
+			connectedPuyos.append_array(puyo.connected)
+			
+		if connectedPuyos.size() > 4:
+			print(connectedPuyos.size())
+			puyosToPop.append_array(connectedPuyos)
+			
+	for puyoToPop in puyosToPop:
+		puyoToPop.pop()
+	checkPopTimer = false
+	
+func findOutAllConnected():
+	pass
