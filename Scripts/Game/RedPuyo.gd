a31aaef913d993b039b70b5b5950c8cd00eb4ac3
Gianfranco <cicciomesere@gmail.com>
2023-08-18 19:10:42 +0100

    Changed naming, puyos store their connected parts as well

--- Scripts/Game/RedPuyo.gd
+++ Scripts/Game/RedPuyo.gd
@@ -1,24 +1,24 @@
 extends Area2D
 
+signal puyoConnected
+
 var tile_size = 58
 
-var moving = false
+var active = false
+var moving = true
 var popped = false
-var type = "RED"
+@export var type = ""
+var connectedPositions = []
 var connected = []
-var connectedCount = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	position = position.snapped(Vector2.ONE * tile_size)
 	position += Vector2.ONE * tile_size/2
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(_delta):
 	if !moving and !popped:
-		searchOtherPuyos()
-		if connectedCount > 3:
-			pop()
+		await searchOtherPuyos()
 
 # 'Gravity' for the puyos that works similarly to the actual games, also stays within the tilemap
 func _on_gravity_timer_timeout():
@@ -28,63 +28,75 @@ func _on_gravity_timer_timeout():
 	else:
 		moving = false
 
+# Searches for other puyos that are connectedPositions, adds to the array if it finds one to update the spritesheet
+# As well as running a recursive function to find out if the other puyos are connectedPositions to others and keeps count
+# If it counts 4 or more it will then pop
 func searchOtherPuyos():
-	connected.clear()
+	connectedPositions.clear()
 	if $RayTop.is_colliding() and $RayTop.get_collider() is Area2D:
-		if $RayTop.get_collider().type == "RED":
-			connected.append("TOP")
+		if $RayTop.get_collider().type == type:
+			connectedPositions.append("TOP")
+			connected.append($RayTop.get_collider())
 	if $RayBottom.is_colliding() and $RayBottom.get_collider() is Area2D:
-		if $RayBottom.get_collider().type == "RED":
-			connected.append("BOTTOM")
+		if $RayBottom.get_collider().type == type:
+			connectedPositions.append("BOTTOM")
+			connected.append($RayBottom.get_collider())
 	if $RayLeft.is_colliding() and $RayLeft.get_collider() is Area2D:
-		if $RayLeft.get_collider().type == "RED":
-			connected.append("LEFT")
+		if $RayLeft.get_collider().type == type:
+			connectedPositions.append("LEFT")
+			connected.append($RayLeft.get_collider())
 	if $RayRight.is_colliding() and $RayRight.get_collider() is Area2D:
-		if $RayRight.get_collider().type == "RED":
-			connected.append("RIGHT")
+		if $RayRight.get_collider().type == type:
+			connectedPositions.append("RIGHT")
+			connected.append($RayRight.get_collider())
+	
 	updateSpriteFrame()
+	
+	if connectedPositions.size() > 0:
+		emit_signal("puyoConnected")
 
-# Update the sprite animation depending on how the its connected to other puyos
+# Update the sprite animation depending on how the its connectedPositions to other puyos
 func updateSpriteFrame():
-	if(connected.size() < 1):
+	if(connectedPositions.size() < 1):
 		$PuyoSprites.play("default")
-	elif(connected.size() == 1 and "TOP" in connected):
+	elif(connectedPositions.size() == 1 and "TOP" in connectedPositions):
 		$PuyoSprites.play("joined_above")
-	elif(connected.size() == 1 and "BOTTOM" in connected):
+	elif(connectedPositions.size() == 1 and "BOTTOM" in connectedPositions):
 		$PuyoSprites.play("joined_below")
-	elif(connected.size() == 1 and "LEFT" in connected):
+	elif(connectedPositions.size() == 1 and "LEFT" in connectedPositions):
 		$PuyoSprites.play("joined_left")
-	elif(connected.size() == 1 and "RIGHT" in connected):
+	elif(connectedPositions.size() == 1 and "RIGHT" in connectedPositions):
 		$PuyoSprites.play("joined_right")
-	elif(connected.size() == 2 and "TOP" in connected and "BOTTOM" in connected):
+	elif(connectedPositions.size() == 2 and "TOP" in connectedPositions and "BOTTOM" in connectedPositions):
 		$PuyoSprites.play("joined_above_below")
-	elif(connected.size() == 2 and "TOP" in connected and "LEFT" in connected):
+	elif(connectedPositions.size() == 2 and "TOP" in connectedPositions and "LEFT" in connectedPositions):
 		$PuyoSprites.play("joined_above_left")
-	elif(connected.size() == 2 and "TOP" in connected and "RIGHT" in connected):
+	elif(connectedPositions.size() == 2 and "TOP" in connectedPositions and "RIGHT" in connectedPositions):
 		$PuyoSprites.play("joined_above_right")
-	elif(connected.size() == 2 and "BOTTOM" in connected and "RIGHT" in connected):
+	elif(connectedPositions.size() == 2 and "BOTTOM" in connectedPositions and "RIGHT" in connectedPositions):
 		$PuyoSprites.play("joined_below_right")
-	elif(connected.size() == 2 and "BOTTOM" in connected and "LEFT" in connected):
+	elif(connectedPositions.size() == 2 and "BOTTOM" in connectedPositions and "LEFT" in connectedPositions):
 		$PuyoSprites.play("joined_below_left")
-	elif(connected.size() == 2 and "LEFT" in connected and "RIGHT" in connected):
+	elif(connectedPositions.size() == 2 and "LEFT" in connectedPositions and "RIGHT" in connectedPositions):
 		$PuyoSprites.play("joined_left_right")
-	elif(connected.size() == 3 and "TOP" in connected and "BOTTOM" in connected and "LEFT" in connected):
+	elif(connectedPositions.size() == 3 and "TOP" in connectedPositions and "BOTTOM" in connectedPositions and "LEFT" in connectedPositions):
 		$PuyoSprites.play("joined_above_below_left")
-	elif(connected.size() == 3 and "TOP" in connected and "BOTTOM" in connected and "RIGHT" in connected):
+	elif(connectedPositions.size() == 3 and "TOP" in connectedPositions and "BOTTOM" in connectedPositions and "RIGHT" in connectedPositions):
 		$PuyoSprites.play("joined_above_below_right")
-	elif(connected.size() == 3 and "TOP" in connected and "LEFT" in connected and "RIGHT" in connected):
+	elif(connectedPositions.size() == 3 and "TOP" in connectedPositions and "LEFT" in connectedPositions and "RIGHT" in connectedPositions):
 		$PuyoSprites.play("joined_above_left_right")
-	elif(connected.size() == 3 and "BOTTOM" in connected and "LEFT" in connected and "RIGHT" in connected):
+	elif(connectedPositions.size() == 3 and "BOTTOM" in connectedPositions and "LEFT" in connectedPositions and "RIGHT" in connectedPositions):
 		$PuyoSprites.play("joined_below_left_right")
 	else:
 		$PuyoSprites.play("joined_all")
 
-# Starts timers to animate the puyo being popped and removes them
+# Starts timers to animate the puyo blinking and popped and removes them
 func pop():
 	popped = true
 	$PuyoSprites.play("popped_start")
 	$PoppedPreTimer.start()
 	$PoppedTimer.start()
+
 
 func _on_popped_pre_timer_timeout():
 	if($PuyoSprites.visible):
