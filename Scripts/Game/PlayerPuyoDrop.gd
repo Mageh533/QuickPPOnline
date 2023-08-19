c759c150b99dd5a2b544c6820c565f4b5c99dc23
Gianfranco Cicciomessere <cicciomesere@gmail.com>
2023-08-19 22:45:20 +0100

    reverting some script changes

--- Scripts/Game/PlayerPuyoDrop.gd
+++ Scripts/Game/PlayerPuyoDrop.gd
@@ -2,21 +2,15 @@ extends Area2D
 
 var tile_size = 58
 var type = "Player"
-@onready var currentGravity = $GravityTimer.wait_time
-@onready var fastDropSpeed = $GravityTimer.wait_time / 2
 
 func _ready():
 	position = position.snapped(Vector2.ONE * tile_size)
 	position += Vector2.ONE * tile_size/2
 
 
-func _process(_delta):
+func _process(delta):
+	position = Vector2.DOWN * delta
 	playerControls()
-
-
-func _on_gravity_timer_timeout():
-	if !$RayLeft.is_colliding() and !$RayRight.is_colliding() and !$RayTLeft.is_colliding() and !$RayTRight.is_colliding() and !$RayBLeft.is_colliding() and !$RayBRight.is_colliding():
-		position += Vector2.DOWN * tile_size
 
 func playerControls():
 	if Input.is_action_just_released("right"):
@@ -26,9 +20,7 @@ func playerControls():
 		if !$RayLeft.is_colliding():
 			position += Vector2.LEFT * tile_size
 	if Input.is_action_pressed("down"):
-		$GravityTimer.wait_time = fastDropSpeed
-	if Input.is_action_just_released("down"):
-		$GravityTimer.wait_time = currentGravity
+		pass
 	if Input.is_action_just_released("turnLeft"):
 		rotate(PI / 2)
 	if Input.is_action_just_released("turnRight"):
