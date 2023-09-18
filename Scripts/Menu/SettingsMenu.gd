d6dcb46bfeff3711a776ae0b0810fd1db68377d8
Gianfranco <cicciomesere@gmail.com>
2023-09-18 14:42:51 +0100

    Commented out settings code
    will leave for later

--- Scripts/Menu/SettingsMenu.gd
+++ Scripts/Menu/SettingsMenu.gd
@@ -2,14 +2,15 @@ extends PopupPanel
 
 # Displays current controls on the buttons
 func _ready():
-	$VerticalContainer/P1Controls1/LeftButton.text
-	$VerticalContainer/P1Controls1/RightButton.text
-	$VerticalContainer/P1Controls1/FastButton.text
-	$VerticalContainer/P1Controls2/LeftTurnButton.text
-	$VerticalContainer/P1Controls2/RightTurnLAbel.text
+	# $VerticalContainer/P1Controls1/LeftButton.text
+	# $VerticalContainer/P1Controls1/RightButton.text
+	# $VerticalContainer/P1Controls1/FastButton.text
+	# $VerticalContainer/P1Controls2/LeftTurnButton.text
+	# $VerticalContainer/P1Controls2/RightTurnLAbel.text
 	
-	$VerticalContainer/P2Controls1/LeftButton.text
-	$VerticalContainer/P2Controls1/RightButton.text
-	$VerticalContainer/P2Controls1/FastButton.text
-	$VerticalContainer/P2Controls2/LeftTurnButton.text
-	$VerticalContainer/P2Controls2/RightTurnLAbel.text
+	# $VerticalContainer/P2Controls1/LeftButton.text
+	# $VerticalContainer/P2Controls1/RightButton.text
+	# $VerticalContainer/P2Controls1/FastButton.text
+	# $VerticalContainer/P2Controls2/LeftTurnButton.text
+	# $VerticalContainer/P2Controls2/RightTurnLAbel.text
+	pass
