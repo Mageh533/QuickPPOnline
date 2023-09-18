bc7c89387fd5e3ff673114135d925f8e8437f6b5
Gianfranco <cicciomesere@gmail.com>
2023-09-18 11:49:32 +0100

    Dont use second player id to check for an online match

--- Scripts/Game/PlayerPuyoDrop.gd
+++ Scripts/Game/PlayerPuyoDrop.gd
@@ -70,10 +70,11 @@ func _process(delta):
 	for rayCast in bottomRaycasts:
 		if rayCast.is_colliding():
 			groundCollide = true
+	
 	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
 		playerControls(1) # if playing multiplayer, use the player 1 controls
 	else:
-		if GameManager.secondPlayerId == 0:
+		if !GameManager.onlineMatch:
 			playerControls(currentPlayer)
 	
 	if groundCollide:
