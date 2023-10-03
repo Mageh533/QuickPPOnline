d3f833ae3b921c68385e8a56b257561845b483e1
Gianfranco <cicciomesere@gmail.com>
2023-10-04 00:41:33 +0100

    Game manager change controls func

--- Scripts/Multiplayer/GameManager.gd
+++ Scripts/Multiplayer/GameManager.gd
@@ -1,8 +1,12 @@
 extends Node
 
-var onlineMatch = false
+# Global variables
 
+var onlineMatch = false
 var fpsCounter = false
+
+var secondPlayerId = 0
+var currentSeed = 0
 
 var soloInfo = {
 	"Mode" : "Endless",
@@ -10,16 +14,18 @@ var soloInfo = {
 	"maxChain" : 0,
 	"speed" : 0
 }
-
 var matchSettings = {
 	"roundsToWin" : 2
 }
-
 var matchInfo = {
 	"matchTime" : 0,
 	"p1Wins" : 0,
 	"p2Wins" : 0
 }
 
-var secondPlayerId = 0
-var currentSeed = 0
+# Global functions
+
+func changeControls(player, control, key):
+	var controlToChange = player + "_" + control
+	InputMap.action_erase_events(controlToChange)
+	InputMap.action_add_event(controlToChange, key)
