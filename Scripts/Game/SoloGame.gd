3e3c99b006e9273c5066b56e74db085343c310a0
Gianfranco <cicciomesere@gmail.com>
2023-10-07 14:38:31 +0100

    show nuisance sent in solo

--- Scripts/Game/SoloGame.gd
+++ Scripts/Game/SoloGame.gd
@@ -4,8 +4,11 @@ signal gameEnd
 
 var matchStarted = false
 
+@onready var nuisanceQueueSprites = $GarbageSentPanel/NuisanceQueue.get_children()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	clearNuisanceQueue()
 	await get_tree().create_timer(0.05).timeout
 	$PuyoGame.process_mode = Node.PROCESS_MODE_DISABLED
 	$UIAnims/AnimationPlayer.play("Start")
@@ -35,3 +38,12 @@ func _on_puyo_game_lost():
 	$UIAnims/AnimationPlayer.play("End")
 	await $UIAnims/AnimationPlayer.animation_finished
 	emit_signal("gameEnd")
+
+func clearNuisanceQueue():
+	for sprite in nuisanceQueueSprites:
+		sprite.hide()
+
+func _on_puyo_game_send_damage(damage):
+	var nuisanceToShow = damage
+	clearNuisanceQueue()
+	GameManager.nuisanceDisplayHelper(nuisanceQueueSprites, nuisanceToShow, 6)
--- Scripts/Menu/MainGame.gd
+++ Scripts/Menu/MainGame.gd
@@ -98,44 +98,8 @@ func clearNuisanceQueue(player):
 func displayNuisanceQueue(player):
 	var nuisanceQueueSprites = get_node(player + "/NuisanceQueue").get_children()
 	var nuisanceToShow = get_node(player).nuisanceQueue
-	var spritesUsed = 0
 	clearNuisanceQueue(player)
-	while nuisanceToShow > 0 and spritesUsed < 6:
-		if nuisanceToShow < 6:
-			nuisanceQueueSprites[spritesUsed].visible = true
-			nuisanceQueueSprites[spritesUsed].play("small")
-			spritesUsed += 1
-			nuisanceToShow -= 1
-		elif nuisanceToShow >= 6 and nuisanceToShow < 30:
-			nuisanceQueueSprites[spritesUsed].visible = true
-			nuisanceQueueSprites[spritesUsed].play("large")
-			spritesUsed += 1
-			nuisanceToShow -= 6
-		elif nuisanceToShow >= 30 and nuisanceToShow < 180:
-			nuisanceQueueSprites[spritesUsed].visible = true
-			nuisanceQueueSprites[spritesUsed].play("rock")
-			spritesUsed += 1
-			nuisanceToShow -= 30
-		elif nuisanceToShow >= 180 and nuisanceToShow < 360:
-			nuisanceQueueSprites[spritesUsed].visible = true
-			nuisanceQueueSprites[spritesUsed].play("star")
-			spritesUsed += 1
-			nuisanceToShow -= 180
-		elif nuisanceToShow >= 360 and nuisanceToShow < 720:
-			nuisanceQueueSprites[spritesUsed].visible = true
-			nuisanceQueueSprites[spritesUsed].play("moon")
-			spritesUsed += 1
-			nuisanceToShow -= 360
-		elif nuisanceToShow >= 720 and nuisanceToShow < 1440:
-			nuisanceQueueSprites[spritesUsed].visible = true
-			nuisanceQueueSprites[spritesUsed].play("crown")
-			spritesUsed += 1
-			nuisanceToShow -= 720
-		elif nuisanceToShow >= 1440:
-			nuisanceQueueSprites[spritesUsed].visible = true
-			nuisanceQueueSprites[spritesUsed].play("comet")
-			spritesUsed += 1
-			nuisanceToShow -= 1440
+	GameManager.nuisanceDisplayHelper(nuisanceQueueSprites, nuisanceToShow, 6)
 
 func playDamageSoundEffects(damage):
 	if damage < 12:
