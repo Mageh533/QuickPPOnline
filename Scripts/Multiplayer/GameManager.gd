2b65ebf780e4eac2003f62ac1b735ee58041b92d
Gianfranco <cicciomesere@gmail.com>
2023-10-07 14:29:10 +0100

    Made the nuisance display func global
    since ill use it in both solo and mp

--- Scripts/Multiplayer/GameManager.gd
+++ Scripts/Multiplayer/GameManager.gd
@@ -29,6 +29,44 @@ var soloMatchSettings = {
 }
 
 # Global functions
+func nuisanceDisplayHelper(nuisanceQueueSprites, nuisanceToShow, spritesToUse):
+	var spritesUsed = 0
+	while nuisanceToShow > 0 and spritesUsed < spritesToUse:
+		if nuisanceToShow < 6:
+			nuisanceQueueSprites[spritesUsed].visible = true
+			nuisanceQueueSprites[spritesUsed].play("small")
+			spritesUsed += 1
+			nuisanceToShow -= 1
+		elif nuisanceToShow >= 6 and nuisanceToShow < 30:
+			nuisanceQueueSprites[spritesUsed].visible = true
+			nuisanceQueueSprites[spritesUsed].play("large")
+			spritesUsed += 1
+			nuisanceToShow -= 6
+		elif nuisanceToShow >= 30 and nuisanceToShow < 180:
+			nuisanceQueueSprites[spritesUsed].visible = true
+			nuisanceQueueSprites[spritesUsed].play("rock")
+			spritesUsed += 1
+			nuisanceToShow -= 30
+		elif nuisanceToShow >= 180 and nuisanceToShow < 360:
+			nuisanceQueueSprites[spritesUsed].visible = true
+			nuisanceQueueSprites[spritesUsed].play("star")
+			spritesUsed += 1
+			nuisanceToShow -= 180
+		elif nuisanceToShow >= 360 and nuisanceToShow < 720:
+			nuisanceQueueSprites[spritesUsed].visible = true
+			nuisanceQueueSprites[spritesUsed].play("moon")
+			spritesUsed += 1
+			nuisanceToShow -= 360
+		elif nuisanceToShow >= 720 and nuisanceToShow < 1440:
+			nuisanceQueueSprites[spritesUsed].visible = true
+			nuisanceQueueSprites[spritesUsed].play("crown")
+			spritesUsed += 1
+			nuisanceToShow -= 720
+		elif nuisanceToShow >= 1440:
+			nuisanceQueueSprites[spritesUsed].visible = true
+			nuisanceQueueSprites[spritesUsed].play("comet")
+			spritesUsed += 1
+			nuisanceToShow -= 1440
 
 func changeControls(control, key):
 	InputMap.action_erase_events(control)
